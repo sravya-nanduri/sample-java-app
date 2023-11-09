711bf02ac6719c637987a8cf31aaab097308d6bc
sravya-nanduri <84439938+sravya-nanduri@users.noreply.github.com>
2023-11-09 10:17:59 +0530

    Update SampleApplication.java
    Signed-off-by: sravya-nanduri <84439938+sravya-nanduri@users.noreply.github.com>

--- src/main/java/io/buildpacks/example/sample/SampleApplication.java
+++ src/main/java/io/buildpacks/example/sample/SampleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleApplication.class, args);
+		// SpringApplication.run(SampleApplication.class, args);
 	}
 
 }
